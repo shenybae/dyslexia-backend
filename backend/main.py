3b7a261b1a767366314cc8b608e1c67b8a8ecf8a
shenybae <shenybae>
2025-12-10 13:41:35 +0800

    Updating

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ import pickle
 
 # Load model, scaler, and label encoder
 model = pickle.load(open("best_mobile_dyslexia_model.pkl", "rb"))
-scaler = pickle.load(open("mbest_mobile_dyslexia_model_scaler.pkl", "rb"))
+scaler = pickle.load(open("best_mobile_dyslexia_model_scaler.pkl", "rb"))
 label_encoder = pickle.load(open("best_mobile_dyslexia_model_labels.pkl", "rb"))
 
 app = FastAPI()
